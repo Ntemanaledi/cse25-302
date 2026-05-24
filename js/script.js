509f6369eb9374ce70659954c88015ba2eda4be6
Ntemanaledi <cse25-302@thuto.bac.ac.bw>
2026-05-24 18:46:32 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function.stringify(cart));function addToCart(car) {
-    updateCartCount();
-    alert(car + " added to cart!");
-}
-
-function updateCartCount() {
-    let count = document.getElementById("cart-count");
-    if (count) {
+function addToCart(car) {
+    cart.push(car); 
+count) {    localStorage.setItem("cart", JSON.stringify(cart));
         count.textContent = cart.length;
     }
 }
+
+
 function searchCars() {
     let input = document.getElementById("searchInput");
 
@@ -22,28 +19,30 @@ function searchCars() {
     window.location.href = "catalog.html?search=" + search;
 }
 
-updateCartCount();
-    cart.push(car);
-function searchCars() {
-    let input = document.getElementById("searchInput");
 
-    if (!input) return;
+function runSearchFromURL() {
+    let params = new URLSearchParams(window.location.search);
+    let search = params.get("search");
 
-    let search = input.value.toLowerCase();
-    let cars = document.querySelectorAll(".car");
+    if (search) {
+        search = search.toLowerCase();
+        let cars = document.querySelectorAll(".car");
 
-    cars.forEach(car => {
-        let text = car.textContent.toLowerCase();
+        cars.forEach(car => {
+            let text = car.textContent.toLowerCase();
 
-        if (text.includes(search)) {
-            car.style.opacity = "1";     
-            car.style.transform = "scale(1.05)";
-        } else {
-            car.style.opacity = "0.3";   
-            car.style.transform = "scale(1)";
-        }
-    });
+            if (text.includes(search)) {
+                car.style.opacity = "1";
+                car.style.transform = "scale(1.05)";
+            } else {
+                car.style.opacity = "0.3";
+                car.style.transform = "scale(1)";
+            }
+        });
+    }
 }
+
+
 let form = document.getElementById("contactForm");
 
 if (form) {
@@ -53,25 +52,14 @@ if (form) {
             "Message sent successfully!";
     });
 }
+
 window.onload = function () {
     updateCartCount();
-
-    let params = new URLSearchParams(window.location.search);
-    let search = params.get("search");
-
-    if (search) {
-        let cars = document.querySelectorAll(".car");
-
-        cars.forEach(car => {
-            let text = car.textContent.toLowerCase();
-
-            if (text.includes(search.toLowerCase())) {
-                car.style.opacity = "1";
-                car.style.transform = "scale(1.05)";
-            } else {
-                car.style.opacity = "0.3";
-                car.style.transform = "scale(1)";
-            }
-        });
-    }
+    runSearchFromURL();
 };
+    updateCartCount();
+    alert(car + " added to cart!");
+}
+
+function updateCartCount() {
+    let count = document.getElementById("cart-count");
